7fb3f52d0988c1d9c4f5d39f7b7403c1f73fca71
George Dittmar <georgedittmar@gmail.com>
2019-06-30 15:10:58 -0700

    adding some examples of reading both txt and json and combining them into 1 dataframe.

--- scripts/SparkMarkovChainGenerator.py
+++ scripts/SparkMarkovChainGenerator.py
@@ -12,13 +12,14 @@ spark = SparkSession.builder.master('local').appName("MarkovChain").getOrCreate(
 
 text_df = PreProcess.bulk_text_distributed_load(spark, '../data_repo/*.txt')
 text_df = text_df.withColumn('text', trim(text_df.text))
+json_df = PreProcess.bulk_json_distributed_load(spark, '../data_repo/*.json').select('text')
 
-print(text_df.show())
+total_df = text_df.union(json_df)
 # tokenize the data and get a new dataframe
 tokenizer = Tokenizer(inputCol='text', outputCol='tokenized_text')
 
 # use sparks built in tokenizer for simplicity and filter out empty new lines from dataset
-tokenized_df = tokenizer.transform(text_df).drop('text')
+tokenized_df = tokenizer.transform(total_df).drop('text')
 tokenized_df = tokenized_df.filter(size('tokenized_text') > 1)
 
 mms = MarkovModelSpark(spark, n=2)
