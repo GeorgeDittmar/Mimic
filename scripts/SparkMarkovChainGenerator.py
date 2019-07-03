747a252dd47aa823be136e76383d700f44444109
George Dittmar <georgedittmar@gmail.com>
2019-07-02 21:37:08 -0700

    some changes

--- scripts/SparkMarkovChainGenerator.py
+++ scripts/SparkMarkovChainGenerator.py
@@ -13,7 +13,6 @@ spark = SparkSession.builder.master('local').appName("MarkovChain").getOrCreate(
 text_df = PreProcess.bulk_text_distributed_load(spark, '../data_repo/*.txt')
 text_df = text_df.withColumn('text', trim(text_df.text))
 json_df = PreProcess.bulk_json_distributed_load(spark, '../data_repo/*.json').select('text')
-
 total_df = text_df.union(json_df)
 # tokenize the data and get a new dataframe
 tokenizer = Tokenizer(inputCol='text', outputCol='tokenized_text')
@@ -21,7 +20,6 @@ tokenizer = Tokenizer(inputCol='text', outputCol='tokenized_text')
 # use sparks built in tokenizer for simplicity and filter out empty new lines from dataset
 tokenized_df = tokenizer.transform(total_df).drop('text')
 tokenized_df = tokenized_df.filter(size('tokenized_text') > 1)
-
 mms = MarkovModelSpark(spark, n=2)
 mms.learn(tokenized_df)
 result = mms.generate()
